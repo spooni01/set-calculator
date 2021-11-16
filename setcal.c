bb22f3bcc3bdcc193170c1517ca5fc6dd7ca9b28
Adam Ližičiar <adamliziciar27@gmail.com>
2021-11-16 19:18:28 +0100

    Implementovaná funkcia na tisk množiny

--- setcal.c
+++ setcal.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-   printf("Hello, World!");
+#define MAX_LENGTH 100
+#define MAX_ROWS 1000
+
+typedef struct set
+{
+   char item[MAX_LENGTH];
+} set_t;
+
+//todo struct relation
+
+void print_set(set_t set[MAX_ROWS])
+{
+   int count = 0;
+
+   printf("{");
+   for(int i = 0; i < MAX_ROWS; i++)
+   {
+      //check if item is empty
+      if(strlen(set[i].item) != 0)
+      {
+         //do not add ',' before first item
+         count++;
+         if(count == 1)
+            printf("%s", set[i].item);
+         else
+            printf(", %s", set[i].item);
+
+      }
+   }
+   printf("}");
+}
+
+int main() 
+{
+   set_t set[MAX_ROWS] = {{"Auto"}, {"pes"}, {"strom"}, {"les"}, {}, {}, {"DF"}};
+   print_set(set);
+
    return 0;
 }
